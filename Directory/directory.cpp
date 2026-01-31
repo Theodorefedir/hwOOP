906c08e04bb92dfc422223f176b79d5ed1fcf63a
Fedir Korolov <korolevfeda2008@gmail.com>
2026-01-31 08:07:58 +0100

    add

---
+++ Directory/directory.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Company {
+public:
+    string name;
+    string owner;
+    string phone;
+    string address;
+    string activity;
+
+    Company() {
+        name = "Unknown";
+        owner = "Unknown";
+        phone = "Unknown";
+        address = "Unknown";
+        activity = "Unknown";
+    }
+    Company(string n, string o, string p, string a, string act) : name(n), owner(o), phone(p), address(a), activity(act) {
+
+    }
+
+    void show() {
+        cout << "Name: " << name << endl;
+        cout << "Owner: " << owner << endl;
+        cout << "Phone: " << phone << endl;
+        cout << "Address: " << address << endl;
+        cout << "Activity: " << activity << endl;
+    }
+};
+
+class Directory {
+private:
+    vector<Company> companies;
+public:
+    void addCompany() {
+        Company newCompany;
+        cout << "Enter company name: ";
+        getline(cin, newCompany.name);
+        cout << "Enter owner: ";
+        getline(cin, newCompany.owner);
+        cout << "Enter phone: ";
+        getline(cin, newCompany.phone);
+        cout << "Enter address: ";
+        getline(cin, newCompany.address);
+        cout << "Enter type of activity: ";
+        getline(cin, newCompany.activity);
+
+        companies.push_back(newCompany);
+        saveToFile();
+    }
+    void showAll() {
+        if (companies.empty()) {
+            cout << "Empty\n";
+            return;
+        }
+        for (int i = 0; i < companies.size(); i++) {
+            companies[i].show();
+        }
+    }
+    void searchByName(string search) {
+        bool found = false;
+        for (int i = 0; i < companies.size(); i++) {
+            if (companies[i].name == search) {
+                companies[i].show();
+                found = true;
+            }
+        }
+
+        if (found == 0) {
+            cout << "Nothing found!\n";
+        }
+    }
+    void searchByPhone(string phoneNumber) {
+        bool found = false;
+        for (int i = 0; i < companies.size(); i++) {
+            if (companies[i].phone == phoneNumber) {
+                companies[i].show();
+                found = true;
+            }
+        }
+
+        if (found == 0) {
+            cout << "Nothing found!\n";
+        }
+    }
+    void saveToFile() {
+        ofstream file("directory.txt");
+        if (file.is_open()) {
+            for (int i = 0; i < companies.size(); i++) {
+                file << companies[i].name << endl;
+                file << companies[i].owner << endl;
+                file << companies[i].phone << endl;
+                file << companies[i].address << endl;
+                file << companies[i].activity << endl;
+            }
+            file.close();
+        }
+    }
+};
+
+int main() {
+    Directory dr;
+    dr.addCompany();
+    dr.showAll();
+}
---
+++ hwAbstraction/Abstraction.cpp
@@ -0,0 +1,176 @@
+﻿//Додати до даного проекта Horse та Elephant зробити це через абстрагування від певних типів.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ITransport
+{
+public:
+	virtual void Drive() = 0;
+};
+
+
+class Auto : public ITransport
+{
+public:
+	void Drive() override
+	{
+		cout << "The car drive on the road.\n";
+	}
+};
+
+
+class Driver
+{
+public:
+	void Travel(ITransport* transport)
+	{
+		transport->Drive();
+	}
+};
+
+// Adaptee - определяет существующий интерфейс, который нуждается в адаптации.
+// интерфейс животного
+class IAnimal
+{
+public:
+	virtual void Move() = 0;
+};
+
+class Camel : public IAnimal
+{
+public:
+	void Move() override
+	{
+		cout << "Camel rides on the sands of the desert.\n";
+	}
+};
+
+// Adapter - адаптирует интерфейс Adaptee к интерфейсу Target.
+// Адаптер от Camel к ITransport
+class CamelToTransportAdapter : public ITransport
+{
+	Camel camel;
+public:
+	CamelToTransportAdapter(Camel animal)
+	{
+		camel = animal;
+	}
+	void Drive()
+	{
+		camel.Move();
+	}
+};
+class Horse : public IAnimal {
+public:
+	void Move() override
+	{
+		cout << "The Horse rides in the forest.\n";
+	}
+};
+class Elephant : public IAnimal {
+public:
+	void Move() override
+	{
+		cout << "An Elephant rides in the jungle.\n";
+	}
+};
+class HorseToTransportAdapter : public ITransport
+{
+	Horse horse;
+public:
+	HorseToTransportAdapter(Horse animal)
+	{
+		horse = animal;
+	}
+	void Drive()
+	{
+		horse.Move();
+	}
+};
+
+class ElephantToTransportAdapter : public ITransport
+{
+	Elephant elephant;
+public:
+	ElephantToTransportAdapter(Elephant animal)
+	{
+		elephant = animal;
+	}
+	void Drive()
+	{
+		elephant.Move();
+	}
+};
+
+
+
+
+class Engine {
+protected:
+	string engineType;
+
+public:
+	Engine() {
+		engineType = "Basic";
+	}
+	Engine(string et) : engineType(et) {}
+
+	virtual void StartEngine() {
+		cout << "Starting " << engineType << endl;
+	}
+
+	virtual ~Engine() {}
+};
+
+class Car : virtual public Engine {
+public:
+	Car() : Engine("Car Engine") {}
+
+	void DriveOnRoad() {
+		StartEngine();
+	}
+};
+
+class Boat : virtual public Engine {
+public:
+	Boat() : Engine("Boat Engine") {}
+
+	void OnWater() {
+		StartEngine();
+	}
+};
+
+class AmphibiousVehicle : public Car, public Boat {
+public:
+	AmphibiousVehicle() : Engine("Amphibious Vehicle Engine") {}
+	void ShowCapabilities() {
+		DriveOnRoad();
+		cout << endl;
+		OnWater();
+	}
+};
+
+int main()
+{
+	AmphibiousVehicle av;
+	av.ShowCapabilities();
+	// мандрівник
+	Driver driver;
+	Auto car;
+	ITransport* transport = &car;
+	// отправляемся в путешествие
+	driver.Travel(transport);
+	// встретились пески, необходимо использовать верблюда
+	Camel camel;
+	Horse horse;
+	// Используем адаптер, который преобразует запрос в один или несколько вызовов к адаптируемому объекту.
+	HorseToTransportAdapter adapter(horse);
+	transport = &adapter;
+	// продолжаем путь по пескам пустыни
+	driver.Travel(transport);
+
+	system("pause");
+	return 0;
+}
